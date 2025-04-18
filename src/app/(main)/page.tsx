5f87fbacf35760cbe488f2bc212e6a19faf19ef9
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2025-04-18 20:52:58 +0200

    Lower ttl for front page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,13 +5,14 @@ import { Speakers } from '@/components/Speakers'
 import { Sponsors } from '@/components/Sponsors'
 import { getConferenceForCurrentDomain } from '@/lib/conference/sanity'
 
-export const revalidate = 3600
+export const revalidate = 60 * 5
 
 export default async function Home() {
   const { conference, error } = await getConferenceForCurrentDomain({
     organizers: false,
     schedule: true,
     sponsors: true,
+    revalidate
   })
 
   if (error) {
