dc14b84695f974bf1ad14cdef88b6690b44ec611
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2025-05-06 21:13:32 +0200

    Fix the formatting for links group

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -59,30 +59,31 @@ export function LinkInput({
   remove: (i: number) => void
 }) {
   return (
-    <div key={name} className="mt-2 flex rounded-md shadow-sm">
-      <div className="relative flex flex-grow items-stretch focus-within:z-10">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <LinkIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-        </div>
+    <div key={name} className="mt-2 flex">
+      <div className="-mr-px grid grow grid-cols-1 focus-within:relative">
         <input
           type="url"
           name={name}
           id={name}
-          className="block w-full rounded-none rounded-l-md bg-white py-1.5 pl-10 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          className="col-start-1 row-start-1 block w-full rounded-l-md bg-white py-1.5 pl-10 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
           value={value}
           onChange={(e) => update(index, e.target.value)}
+        />
+        <LinkIcon
+          aria-hidden="true"
+          className="pointer-events-none col-start-1 row-start-1 ml-3 size-5 self-center text-gray-400 sm:size-4"
         />
       </div>
       <button
         type="button"
-        className="relative -ml-px inline-flex items-center gap-x-1.5 px-3 py-2 text-sm font-semibold text-gray-900 outline-1 outline-gray-300 hover:bg-gray-50"
+        className="flex shrink-0 items-center gap-x-1.5 bg-white px-3 py-2 text-sm font-semibold text-gray-900 outline-1 -outline-offset-1 outline-gray-300 hover:bg-gray-50 focus:relative focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
         onClick={() => remove(index)}
       >
         <MinusCircleIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
       </button>
       <button
         type="button"
-        className="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 outline-1 outline-gray-300 hover:bg-gray-50"
+        className="flex shrink-0 items-center gap-x-1.5 rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 outline-1 -outline-offset-1 outline-gray-300 hover:bg-gray-50 focus:relative focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
         onClick={() => add(index)}
       >
         <PlusCircleIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
