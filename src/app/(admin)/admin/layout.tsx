b41b2a127f57a3c56b7649dee99191fce9e5271e
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2025-06-10 14:11:14 +0200

    Check the user

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,9 +1,19 @@
 import { AdminLayout } from '@/components/admin'
+import { auth } from '@/lib/auth'
 
 export default async function AdminRootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await auth()
+  if (!session || !session.speaker || !session.speaker.is_organizer) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p className="text-lg text-gray-500">Access Denied</p>
+      </div>
+    )
+  }
+
   return <AdminLayout>{children}</AdminLayout>
 }
