873124f09c8f077e7fbab9dba6fc838c184324ec
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2026-03-30 21:06:17 +0200

    fix: prevent multiple registrations of event handlers

--- src/lib/events/registry.ts
+++ src/lib/events/registry.ts
@@ -4,15 +4,18 @@ import { handleSlackNotification } from './handlers/slackNotification'
 import { handleAudienceUpdate } from './handlers/audienceUpdate'
 import { handleGalleryTagNotification } from './handlers/galleryTagNotification'
 
+let registered = false
+
 export function registerEventHandlers(): void {
+  if (registered) return
+  registered = true
+
   eventBus.subscribe('proposal.status.changed', handleEmailNotification)
   eventBus.subscribe('proposal.status.changed', handleSlackNotification)
   eventBus.subscribe('proposal.status.changed', handleAudienceUpdate)
 
   // Register gallery speaker tagged handler
   eventBus.subscribe('gallery.speaker.tagged', handleGalleryTagNotification)
-
-  console.log('Event handlers registered successfully')
 }
 
 registerEventHandlers()
